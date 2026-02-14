df90bbe2ef148321bb080b2c2f083cb1b04bff57
ashizawakazuki <ashizawa.kazuki@gmail.com>
2026-02-15 00:55:36 +0900

    各国の国の一覧を仮で作成

--- action/national_teams.php
+++ action/national_teams.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/../inc/config.php';
 
+$countries = CountryRepository::getCountry();
 
 
 $smarty->assign('filename', 'national_teams.html');
+$smarty->assign('countries', $countries);
 $smarty->display('template.html');
