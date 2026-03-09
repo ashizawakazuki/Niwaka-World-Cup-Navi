6fc9d7b8412d328cd618e56e0a7aac6b0d62be5d
ashizawakazuki <ashizawa.kazuki@gmail,com>
2026-03-09 21:38:02 +0900

    Player.phpを作成し、日本チームの画面を作った

--- action/japan_team.php
+++ action/japan_team.php
@@ -1,5 +1,10 @@
 <?php
 require_once __DIR__ . '/../inc/config.php';
 
+use class\player\PlayerRepository;
+//④裏側のファイルでクラスからオブジェクトを持ってくる
+$players = PlayerRepository::getJapanPlayer();
+
 $smarty->assign('filename', 'japan_team.html');
+$smarty->assign('players', $players);
 $smarty->display('template.html');
---
+++ database/20260308_create_table_japan_players.sql
@@ -0,0 +1,3 @@
+-- テーブル作成したときのカラムは書き忘れた
+
+INSERT INTO `japan_players` (`id`, `name`, `number`, `position`, `club`, `age`, `height`, `weight`, `image_path`, `highlight_url`) VALUES (NULL, '鈴木彩艶', '1', 'ゴールキーパー', 'パルマ・カルチョ（イタリア）', '23', '191', '98', 'suzuki_zion.jpg', 'https://www.youtube.com/watch?v=yw6EIMDOhwg');
