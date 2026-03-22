df6802a72e0e6e9195b7f1c5a574da14393b9b7e
ashizawakazuki <ashizawa.kazuki@gmail,com>
2026-03-22 15:30:34 +0900

    日本代表のロジックにtry-catchを記述

--- action/japan_team.php
+++ action/japan_team.php
@@ -3,7 +3,22 @@ require_once __DIR__ . '/../inc/config.php';
 
 use class\player\PlayerRepository;
 //④裏側のファイルでクラスからオブジェクトを持ってくる
-$players = PlayerRepository::getJapanPlayer();
+
+try {
+    $players = PlayerRepository::getJapanPlayer();
+    if (empty($players)) {
+        throw new PDOException("データベース関係でエラーが出ています");
+    }
+
+    // Exceptionクラスのオブジェクトが$eに入っている、そしてそのオブジェクトのメソッドをつかってメッセージなどを出している
+} catch(PDOException $e) {
+    // よく使われるメソッド上位3つ（AIより）
+    echo $e->getMessage()."<br>"; //エラーです
+    echo $e->getFile()."<br>"; // /var/www/html/class/player/PlayerRepository.php
+    echo $e->getLine()."<br>"; // 17
+    echo $e->getFile()."の".$e->getLine()."行目で".$e->getMessage();
+    exit; //処理をここで終わらせる(HTMLファイルまでいかないようにしている）
+}
 
 $smarty->assign('filename', 'japan_team.html');
 $smarty->assign('players', $players);
