bbd831ada9724f66eefebadbbedce6e0b1cae5a5
ashizawakazuki <ashizawa.kazuki@gmail,com>
2026-05-06 19:17:55 +0900

    結果のファイル作成

--- action/tamai_quiz/result.php
+++ action/tamai_quiz/result.php
@@ -1,1 +1,15 @@
 <?php
+
+session_start();
+
+$score = $_SESSION['score'] ?? 0;
+$total_question_count = count($_SESSION['quiz_questions'] ?? []);
+
+$score_point = $score * 20;
+
+$smarty->assign('filename', 'tamai_quiz/result.html');
+$smarty->assign('score', $score);
+$smarty->assign('score_point', $score_point);
+$smarty->assign('total_question_count', $total_question_count);
+
+$smarty->display('tamai_template.html');
